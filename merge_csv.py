258df488c19e1a41c11fcd45862f327fbbc17305
Sagarika Ambast <75776367+AmbastSagarika@users.noreply.github.com>
2022-10-12 19:34:17 +0530

    Update merge_csv.py

--- merge_csv.py
+++ merge_csv.py
@@ -24,6 +24,6 @@ for movie_item in all_movies:
             if movie_item[8] == movie_link_item[0]:
                 movie_item.append(movie_link_item[1])
                 if len(movie_item) == 28:
-                    with open("final1.csv", "a+",encoding='utf-8') as final:
+                    with open("final.csv", "a+",encoding='utf-8') as final:
                         csvwriter = csv.writer(final)
                         csvwriter.writerow(movie_item)
